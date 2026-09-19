3b85a141271751db46ceb0263e8c347c7c22869d
ArneNuyts <arnenuyts@outlook.be>
2026-09-19 08:59:05 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -113,84 +113,84 @@ const samplerConfig = [
          x: 60,
          y: 45
    },
-   // {
-     //    gif: 'gifs/car.webp',
-     //    staticImg: 'gifs/car.png',
-     //    audio: 'audio/car.wav',
-     //    width: 120,
-     //    height: 120,
-     //    x: 60,
-     //    y: 45
-    //},
-    //{
-         //gif: 'gifs/centi.webp',
-         //staticImg: 'gifs/centi.png',
-         //audio: 'audio/centi1.wav',
-         //width: 120,
-         //height: 120,
-         //x: 60,
-         //y: 45
-    //},
-    //{
-     //    gif: 'gifs/tornado.webp',
-     //   staticImg: 'gifs/tornado.png',
-     //    audio: 'audio/tornado1.wav',
-      //   width: 100,
-      //   height: 100,
-      //   x: 60,
-      //   y: 45
-   // },
-   // {
-     //    gif: 'gifs/flower-web.webp',
-     //    staticImg: 'gifs/flower-web.png',
-     //    audio: 'audio/tornado3.wav',
-     //    width: 100,
-     //    height: 100,
-     //    x: 60,
-     //    y: 45
-   // },
-    // {
-      //  gif: 'gifs/sun2.webp',     // Path to GIF
-      //  staticImg: 'gifs/sun2.png', // Path to static image (first frame)
-      //  audioList: [                       // Random sample picked on each trigger
-         //   'audio/sun-samples/sun1.wav',
-        //    'audio/sun-samples/sun2.wav',
-         //   'audio/sun-samples/sun3.wav',
-         //   'audio/sun-samples/sun4.wav',
+    {
+        gif: 'gifs/car.webp',
+         staticImg: 'gifs/car.png',
+         audio: 'audio/car.wav',
+         width: 120,
+         height: 120,
+         x: 60,
+         y: 45
+    },
+    {
+         gif: 'gifs/centi.webp',
+         staticImg: 'gifs/centi.png',
+         audio: 'audio/centi1.wav',
+         width: 120,
+         height: 120,
+         x: 60,
+         y: 45
+    },
+    {
+         gif: 'gifs/tornado.webp',
+        staticImg: 'gifs/tornado.png',
+     audio: 'audio/tornado1.wav',
+       width: 100,
+       height: 100,
+        x: 60,
+       y: 45
+  },
+  {
+     gif: 'gifs/flower-web.webp',
+     staticImg: 'gifs/flower-web.png',
+     audio: 'audio/tornado3.wav',
+     width: 100,
+     height: 100,
+     x: 60,
+     y: 45
+   },
+    {
+      gif: 'gifs/sun2.webp',     // Path to GIF
+      staticImg: 'gifs/sun2.png', // Path to static image (first frame)
+      audioList: [                       // Random sample picked on each trigger
+        'audio/sun-samples/sun1.wav',
+        'audio/sun-samples/sun2.wav',
+         'audio/sun-samples/sun3.wav',
+         'audio/sun-samples/sun4.wav',
 
-       // ],
-      //  width: 112,                        // Width in pixels
-      //  height: 112,                       // Height in pixels
-      //  x: 50,                             // Initial X position (%)
-      //  y: 20                              // Initial Y position (%)
-   // },
-    //{
-         //gif: 'gifs/seed-30f.webp',
-         //staticImg: 'gifs/seed-30f.png',
-         //audio: 'audio/mono2.wav',
-         //width: 100,
-         //height: 100,
-         //x: 60,
-         //y: 45
-   // },
-   // {
-        // gif: 'gifs/seed-canva1.webp',
-        // staticImg: 'gifs/seed-canva1.png',
-        // audio: 'audio/seed1.wav',
-        // width: 100,
-        // height: 100,
-        // x: 60,
-        // y: 45
- //   },
-    // {
-    //     gif: 'gifs/blue_dolphin.webp',
-    //     staticImg: 'gifs/blue_dolphin.png',
-    //     audio: 'audio/dans3.m4a',
-    //     width: 112,
-    //     height: 112,
-    //     x: 40,
-    //     y: 75
-    // }
+      ],
+      width: 112,                        // Width in pixels
+      height: 112,                       // Height in pixels
+       x: 50,                             // Initial X position (%)
+      y: 20                              // Initial Y position (%)
+  },
+    {
+         gif: 'gifs/seed-30f.webp',
+         staticImg: 'gifs/seed-30f.png',
+         audio: 'audio/mono2.wav',
+         width: 100,
+         height: 100,
+         x: 60,
+         y: 45
+    },
+  {
+        gif: 'gifs/seed-canva1.webp',
+        staticImg: 'gifs/seed-canva1.png',
+        audio: 'audio/seed1.wav',
+        width: 100,
+        height: 100,
+        x: 60,
+        y: 45
+},
+    {
+    gif: 'gifs/blue_dolphin.webp',
+    staticImg: 'gifs/blue_dolphin.png',
+    audio: 'audio/dans3.m4a',
+    width: 112,
+    height: 112,
+    x: 40,
+    y: 75
+    }
 ];
 
 // Detect if device is mobile
